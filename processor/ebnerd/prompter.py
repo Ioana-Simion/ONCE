84d6777b326e5c88d8e8907843e644ba0d9cade2
AnaMVasilcoiu <ana.vasilcoiu@gmail.com>
2024-06-18 17:44:30 +0200

    Fix user profiler for large

--- processor/ebnerd/prompter.py
+++ processor/ebnerd/prompter.py
@@ -83,7 +83,7 @@ class EbnerdUser:
             return self._user_list
         self._user_list = []
 
-        for _, history in tqdm(self.history_df.iloc.iterrows()):
+        for _, history in tqdm(self.history_df.iloc[:, :].iterrows()):
             string = ""
 
             # Add user characteristics (from behavior)
@@ -137,41 +137,3 @@ class EbnerdColdUser:
 
             self._user_list.append((history["user_id"], string))
         return self._user_list
-
-
-# class MindCoT:
-#     def __init__(self, data_path, plugin_path, mind_prompter, allowed_user_path):
-#         self.depot = UniDep(data_path, silent=True)
-#         self.plugin = UniDep(plugin_path, silent=True)
-#         self.tv = self.plugin.vocabs["topic"]
-#         self.rv = self.plugin.vocabs["region"]
-
-#         self.nid = self.depot.vocabs("nid")
-#         self.news_dict = mind_prompter.get_news_dict_with_category()
-
-#         self._user_list = None
-#         self.allowed_user = json.load(open(allowed_user_path))
-
-#     def stringify(self):
-#         if self._user_list is not None:
-#             return self._user_list
-#         self._user_list = []
-#         for user in tqdm(self.depot):
-#             if user["uid"] not in self.allowed_user:
-#                 continue
-#             string = ""
-#             pg = self.plugin[user["uid"]]
-#             string += "Interest Topics:\n"
-#             for t in pg["topic"]:
-#                 string += f"- {self.tv[t]}\n"
-#             string += "\n"
-#             # string += 'Interest Regions:\n'
-#             # for r in pg['region']:
-#             #     string += f'- {self.rv[r]}\n'
-#             # string += '\n'
-
-#             string += "History:\n"
-#             for i, n in enumerate(user["history"]):
-#                 string += f"({i + 1}) {self.news_dict[self.nid.i2o[n]]}\n"
-#             self._user_list.append((user["uid"], string))
-#         return self._user_list
