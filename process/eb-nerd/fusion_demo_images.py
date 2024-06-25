fb5f747bb6e4393e463e08d98253337ca8faf0ec
AnaMVasilcoiu <ana.vasilcoiu@gmail.com>
2024-06-25 20:12:52 +0200

    Add fusion demo + images file

---
+++ process/eb-nerd/fusion_demo_images.py
@@ -0,0 +1,21 @@
+from UniTok import UniDep
+import numpy as np
+import os
+
+news = UniDep(os.path.join("ebnerd-benchmark/data/ebnerd_demo/tokenized_bert", 'news'))
+news_llama = UniDep('ebnerd-benchmark/data/ebnerd_demo/tokenized_llama/news-llama')
+
+news.rename_col('title', 'title-bert')
+news.rename_col('subtitle', 'subtitle-bert')
+news.rename_col('body', 'body-bert')
+news.rename_col('category', 'category-token')
+news.rename_col('image_caption_text', 'image_caption_text_token')
+
+news_llama.rename_col('title', 'title-llama')
+news_llama.rename_col('subtitle', 'subtitle-llama')
+news_llama.rename_col('body', 'body-llama')
+news_llama.rename_col('category', 'category-llama')
+news_llama.rename_col('image_caption_text', 'image_caption_text_llama')
+
+news.inject(news_llama, ['title-llama', 'subtitle-llama', 'body-llama', 'category-llama', 'image_caption_text_llama'])
+news.export('ebnerd-benchmark/data/ebnerd_demo/news_fusion')
