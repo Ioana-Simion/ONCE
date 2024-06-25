4128352ef49fc0d69563f11108801c3be51cec06
AnaMVasilcoiu <ana.vasilcoiu@gmail.com>
2024-06-25 16:52:20 +0200

    Add processors for demo

---
+++ process/eb-nerd/fusion_demo.py
@@ -0,0 +1,19 @@
+from UniTok import UniDep
+import numpy as np
+import os
+
+news = UniDep(os.path.join("ebnerd-benchmark/data/ebnerd_demo/tokenized_bert", 'news'))
+news_llama = UniDep('ebnerd-benchmark/data/ebnerd_demo/tokenized_llama/news-llama')
+
+news.rename_col('title', 'title-bert')
+news.rename_col('subtitle', 'subtitle-bert')
+news.rename_col('body', 'body-bert')
+news.rename_col('category', 'category-token')
+
+news_llama.rename_col('title', 'title-llama')
+news_llama.rename_col('subtitle', 'subtitle-llama')
+news_llama.rename_col('body', 'body-llama')
+news_llama.rename_col('category', 'category-llama')
+
+news.inject(news_llama, ['title-llama', 'subtitle-llama', 'body-llama', 'category-llama'])
+news.export('ebnerd-benchmark/data/ebnerd_demo/news_fusion')
