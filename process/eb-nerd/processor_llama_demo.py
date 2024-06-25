4de0461c41785e4aa619c098db8cc8a9a24032eb
AnaMVasilcoiu <ana.vasilcoiu@gmail.com>
2024-06-25 18:19:09 +0200

    Use preprocessed in demo

--- process/eb-nerd/processor_llama_demo.py
+++ process/eb-nerd/processor_llama_demo.py
@@ -30,7 +30,7 @@ class Processor:
         self.data_dir = data_dir
         self.store_dir = store_dir
 
-        self.news_path = os.path.join(self.data_dir, 'preprocessed_and_title_enhanced.parquet')
+        self.news_path = os.path.join(self.data_dir, 'preprocessed.parquet')
 
         self.nid = Vocab(name='nid')
         self.uid = Vocab(name='uid')
